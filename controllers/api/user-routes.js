1fdd7f0132c9e5317f724c7d30466b25202e9992
bm <marcusb733@gmail.com>
2022-06-21 22:40:46 -0700

    Add StyleSheet

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -65,13 +65,13 @@ router.post('/', (req, res) => {
         req.session.user_id = dbUserData.id;
         req.session.username = dbUserData.username;
         req.session.loggedIn = true;
-        
+  
         res.json(dbUserData);
       });
     })
     .catch(err => {
       console.log(err);
-      res.status(500).json(er--r);
+      res.status(500).json(err);
     });
 });
 
@@ -95,12 +95,11 @@ router.post('/login', (req, res) => {
     }
 
     req.session.save(() => {
-      //declare session variables
       req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
-
-      res.json({ user: dbUserData, message: 'You are now logged in!' }); 
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
   });
 });
@@ -117,6 +116,8 @@ router.post('/logout', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
     individualHooks: true,
@@ -125,7 +126,7 @@ router.put('/:id', (req, res) => {
     }
   })
     .then(dbUserData => {
-      if (!dbUserData[0]) {
+      if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,3 @@
-DROP DATABASE IF EXISTS newsfeed_db;
+DROP DATABASE IF EXISTS technews_db;
 
-CREATE DATABASE newsfeed_db;
+CREATE DATABASE technews_db;
